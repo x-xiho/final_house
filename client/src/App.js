5a5642f809fdb3720a13881a0fd58dd60fb738a6
x-xiho <x_xiho3o@naver.com>
2023-10-05 17:07:41 +0900

    client 설치

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+import Nav from './components/Nav'
+import Main from './components/Main'
+import Myhome from './components/Myhome'
+import Test from './Test'
+
+import Login from './components/Login'
+import Mypage from './components/Mypage'
+import Page1 from './components/pages/Page1'
+import Page2 from './components/pages/Page2'
+import Page3 from './components/pages/Page3'
+import PageEnd from './components/pages/PageEnd'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Nav />}>
+          <Route path="/" element={<Main />} />
+
+          <Route path="myhome" element={<Myhome />}>
+            <Route index element={<Page1 />} />
+            <Route path="page2" element={<Page2 />} />
+            <Route path="page3" element={<Page3/>} />
+            <Route path="pageEnd" element={<PageEnd/>} />
+          </Route>
+
+          <Route path="test" element={<Test />} />
+          <Route path="login" element={<Login />} />
+          <Route path="mypage" element={<Mypage />} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
